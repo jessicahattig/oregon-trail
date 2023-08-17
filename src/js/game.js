24ce80b44ef622fbbf22db8ebb3f6ae91c9202bc
jessicahattig <jessicahattig@gmail.com>
2023-08-16 18:56:56 -0700

    WIP: game.

---
+++ src/js/game.js
@@ -0,0 +1,36 @@
+class Narrative {
+  constructor() {
+    this.storyData = [
+      {
+        text: "EcoGuard: Corporate Takedown You are Alex, a young eco-warrior standing against the environmentally destructive Monsanto Corporation. Start: You're in the heart of the city, watching as smog clouds the skyline. Monsanto's logo is everywhere,",
+        choices: [
+          {text: " Sneak into a Monsanto facility to gather information?", nextStepA: 1 },
+          {text: "Seek out the eco-resistance in the nearby forest?", nextStepB: 2 }
+        ]
+      },
+      {
+        text: "Sneak into Monsanto facility: You manage to bypass security and enter the facility, but it's crawling with guards.",
+        choices: [
+          {text: "Try to hack into their mainframe for evidence?", nextStepA: 3},
+          {text: "Look for a physical file or documents?", nextStepB:4 }
+        ]
+      },
+      
+    ]
+  }
+}: {},
+  narrative:   "EcoGuard: Corporate Takedown You are Alex, a young eco-warrior standing against the environmentally destructive Monsanto Corporation. Start: You're in the heart of the city, watching as smog clouds the skyline. Monsanto's logo is everywhere,",
+  option1: {
+    narrative: "Sneak into a Monsanto facility to gather information",
+    option1: {},
+    option2: {},
+  },
+  option2: {
+    narrative: "Seek out the eco-resistance in the nearby forest",
+    option1: {},
+    option2: {},
+  },
+start: {},
+  narrative: Text,
+  option1: {},
+  option2: {},
